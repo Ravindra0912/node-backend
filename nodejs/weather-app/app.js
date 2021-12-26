6c558af1c919d577b9d13829d123dbdf89055af1
Ravindra Shrivastava <ravindrashrivastava@pop-os.localdomain>
2021-12-26 15:44:41 +0530

    more changes

--- nodejs/weather-app/app.js
+++ nodejs/weather-app/app.js
@@ -2,8 +2,6 @@ const request = require('request');
 
 const url = 'http://api.weatherstack.com/current?access_key=9406de3bd3e9bd3452a5eab3b9fbc261&query=37.8267,-122.4233';
 
-request({ url, json: true },(error,response)=>{
-    // console.log(response)
-    // const data = JSON.parse(response.body)
-    console.log(response.body.current)
-})
+request({ url, json: true }, (error, response) => {
+    console.log(response.body.current);
+});
--- nodejs/web-server/src/app.js
+++ nodejs/web-server/src/app.js
@@ -1,7 +1,13 @@
 const path = require('path');
 const express = require('express');
 const hbs = require('hbs');
+const request = require('request');
 
+const url = 'http://api.weatherstack.com/current?access_key=9406de3bd3e9bd3452a5eab3b9fbc261&query=37.8267,-122.4233';
+
+request({ url, json: true }, (error, response) => {
+    console.log(response.body.current);
+});
 
 console.log(__dirname);
 console.log(path.join(__dirname,'../public'));
